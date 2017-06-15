c60acc1ee198aa55817803afafb3807211d6c140
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-06-15 15:48:04 -0400

    Add basic jQuery animations

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function() {
+            $("#fadeOut").click(function() {
+                $("#project1").fadeOut("normal");
+            });
+            $("#fadeIn").click(function() {
+                $("#project1").fadeIn("normal");
+            });
+        });
