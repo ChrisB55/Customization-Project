d2a2454a57ae11850e4d59e2b56089bee15abc70
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-06-29 19:48:56 -0400

    Add panel markup, styling

--- js/app.js
+++ js/app.js
@@ -5,8 +5,28 @@ $(function() {
             $("#fadeIn").click(function() {
                 $("#project1").fadeIn("normal");
             });
+            
+            
         });
+$(document).ready(function(){
+    panel_go();
+}
+);
 
+function panel_go() {
+
+    $('.info_panel').each(function(){
+
+        var link_text = $(this).attr('data-link-text');
+        var content = $(this).html();
+
+        $(this).html('<div class="info_panel_content_container"> <div class="info_panel_content">'+content+'</div></div>');
+
+
+        $(this).append('<div class="info_panel_trigger">' + link_text+'</div>');
+
+    });
+}
 
     /*$(".col-4-projects").hover(function() {
         $("#project1").animate({ 
